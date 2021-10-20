7b5738bc0a4177018f87db89ae4c5f00efaa56c8
Nathan Lee <nslee333@gmail.com>
2021-10-19 18:42:54 -0700

    Commit #3

---
+++ Learning1.js
@@ -0,0 +1,44 @@
+// Example.
+
+var firstLetterOfFirstName = '';
+var firstName = 'Ada';
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup.
+
+var firstLetterOfLastName = '';
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+firstLetterOfLastName = lastName[1];
+console.log(firstLetterOfLastName);
+
+// Computers start counting with 0, this is called Zero Based Indexing.
+// Bracket based Indexing helps with finding a specfic part of a string getting specific information
+// from an array.
+
+
+// String Immutability.
+
+// Strings are Immutable. Meaning they cannot be altered once they are created.
+// This does not mean they cannot be changed, just that the individual characters of a string 
+// literal cannot be changed.
+
+
+// The code below is attempting to use bracket notation to change the 
+// first character in the string, but it will throw an error because of the 
+// immutability of the string.
+
+// You cannot change the individual characters of the string, but you can change the whole string itself.
+
+// Setup.
+
+var myStr = "Jello World";
+
+// Only change the code below this line.
+
+myStr[0] = 'H'; // Fix me.
+
+//
