6a47ad992983e305249f1b68b6a3d23ef347ad14
Nathan Lee <nslee333@gmail.com>
2021-10-20 19:16:44 -0700

    Commit

---
+++ LearningOOP.js
@@ -0,0 +1,19 @@
+// Obect Oriented Programming is a programming paradigm centered on objects rather than functions.
+// OOP is a programming paradigm.
+// Languages that supprot OOP, such as  C#, Java, Ruby, Python, Javascript etc.
+
+// There are some frameworks that use OOP concepts, Angular.js is one of those frameworks.
+//
+
+// This course we will go over learning and understanding OOP principles, and how to implement them in JS.
+
+// The four pillars of OOP.
+
+// Encapsulation
+
+// Abstraction.
+
+// Inheritence.
+
+// Polymorphism.
+
